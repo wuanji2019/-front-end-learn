5944085b1cf959a7a7c23fc2f264426949a09033
An7l <331531027@qq.com>
2020-08-26 17:38:58 +0800

    add some thing

---
+++ debounce/debonunce.js
@@ -0,0 +1,50 @@
+function debounce(fn,wait) {
+    var timeout = null;
+    return function() {
+        if(timeout !==null)
+         clearTimeout(timeout);
+        timeout = setTimeout(fn,wait);
+    }
+}
+function handle() {
+    console.log(Math.random())
+}
+// 滚动事件
+window.addEventListener('scroll', debounce(handle, 1000));
+
+
+var throttle = function(func, delay) {
+    var prev = Date.now();
+    return function() {
+        var context = this;
+        var args = arguments;
+        var now = Date.now();
+        if (now - prev >= delay) {
+            func.apply(context, args);
+            prev = Date.now();
+        }
+    }
+}
+function handle() {
+    console.log(Math.random());
+}
+window.addEventListener('scroll', throttle(handle, 1000));
+
+
+var throttle = function(func, delay) {
+    var timer = null;
+    return function() {
+        var context = this;
+        var args = arguments;
+        if (!timer) {
+            timer = setTimeout(function() {
+                func.apply(context, args);
+                timer = null;
+            }, delay);
+        }
+    }
+}
+function handle() {
+    console.log(Math.random());
+}
+window.addEventListener('scroll', throttle(handle, 1000));
